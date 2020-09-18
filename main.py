d02b6e9890a48fa642d02bb3b04e334d522da142
Neutrino <srinivasanm329@gmail.com>
2020-09-18 23:14:44 +0530

    Main window with required UI

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import tkinter as tk
+from tkinter import ttk 
+import cv2
+from PIL import ImageGrab
+import numpy as np
+import math
+import serial
+import serial.tools.list_ports
+import time
+import pyautogui
 
 class InteractiveScreenshot(tk.Frame):
     def __init__( self, parent):
@@ -18,16 +27,46 @@ class InteractiveScreenshot(tk.Frame):
         self.recty1 = 0
         self.rectid = None
 
+    def openNewWindow(self): 
+        # Toplevel object which will  
+        # be treated as a new window 
+        self.newWindow = tk.Toplevel() 
+  
+        # sets the title of the 
+        # Toplevel widget 
+        self.newWindow.title("New Window") 
+  
+        # sets the geometry of toplevel 
+        self.newWindow.geometry("200x200") 
+  
+        # A Label widget to show in toplevel 
+        self.Label(self.newWindow,text ="This is a new window").pack() 
+        self.parent.destroy()
+
+
     def selectionDone(self):
         print('started at x = {1} y = {2} ended at x1 = {3} y1 = {4} '. format(self.rectid, self.rectx0, self.recty0, self.rectx1,
                      self.recty1))
+        '''Getting the Image from the screen and find the edge of the health bar'''
+        print(pyautogui.position())
+	    #Grabbing the image from screen using pillow library
+	    #converting the image to numpy array to use the image with cv2 library
+        screen = np.array(ImageGrab.grab(bbox = (self.rectx0-0.5,self.recty0+26,self.rectx1,self.recty1+26.5)))
+        cvt_image = cv2.cvtColor(screen,cv2.COLOR_BGR2RGB)
+	    #using open cv2 to gray out the image grab
+        gray_image = cv2.cvtColor(cvt_image, cv2.COLOR_BGR2GRAY)
+
+        #blur the gray image to remove noise and to averaging the color
+        blurred = cv2.bilateralFilter(gray_image,15,65,65)
+    	#displaying the edge and thresh image
+        cv2.imshow('Original',cvt_image)
 
     def _createCanvas(self):
         self.canvas = tk.Canvas(self.parent, width = root.winfo_screenwidth(), height = root.winfo_screenheight(),
                                 bg = None)
         #self.canvas.grid(row=0, column=0)
         self.mainScreenButton = tk.Button(root, text="Main Menu", width=10, command=self.quit(),bg='#00ffea',fg='black')
-        self.screenshotButton = tk.Button(root, text="Grab", width=10, command=self.selectionDone,bg='#00ff08',fg='black')
+        self.screenshotButton = tk.Button(root, text="Grab", width=10, command=self.openNewWindow,bg='#00ff08',fg='black')
         #self.button.grid(padx = 50,pady=50,row=2, column=1)
         self.screenshotButton.grid(padx = 5,row=0, column=1, sticky='E')
         self.mainScreenButton.grid(padx = 5,row=0, column=0, sticky= 'W')
@@ -68,6 +107,7 @@ class InteractiveScreenshot(tk.Frame):
         self.canvas.coords(self.rectid, self.rectx0, self.recty0,
                       self.rectx1, self.recty1)
         print('Rectangle ended')
+        print(pyautogui.position())
 
     def toggle_fullscreen(self, event=None):
         self.state = not self.state  # Just toggling the boolean
@@ -79,14 +119,81 @@ class InteractiveScreenshot(tk.Frame):
         self.tk.attributes("-fullscreen", False)
         return "break"
 
-def helloCallBack():
-    pass
+def  Image_processing():
+    	pass
+
+def serial_ports():
+    ports = serial.tools.list_ports.comports()
+    print('Serial searching')
+    result = []
+    for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
+        result.append(desc)
+    return result
+
+def refreshPorts():
+    comList = serial_ports()
+    if not comList:
+        comList.append("No device")
+    com['values'] = comList
+    com.current(0)
+
 
 if __name__ == "__main__":
+
+    baduRate = ["9600","115200","19200"]
+
     root = tk.Tk()
-    root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
-    root.attributes('-alpha', 0.3)
-    root.attributes("-fullscreen", True)
-    app = InteractiveScreenshot(root)
+    #root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+    #root.attributes('-alpha', 0.3)
+    #root.attributes("-fullscreen", True)
+    #app = InteractiveScreenshot(root)
+    root.title("Gamer Assist");
+    root.geometry("315x175");
+
+    # Adding Menu system
+    menu_system = tk.Menu(root)
+    root.config(menu=menu_system)
+
+    #creating file menu item
+    file_menu = tk.Menu(menu_system)
+    menu_system.add_cascade(label="File",menu=file_menu)
+    file_menu.add_command(label="New Config",command=None)
+    file_menu.add_command(label="Open Config",command=None)
+    file_menu.add_command(label="Save Config",command=None)
+    file_menu.add_separator()
+    file_menu.add_command(label="Exit",command=root.quit)
+
+    #creating help menu item
+    help_menu = tk.Menu(menu_system)
+    menu_system.add_cascade(label="Help",menu=help_menu)
+    help_menu.add_command(label="Manual",command=None)
+    help_menu.add_command(label="About",command=None)
+
+
+    tk.Canvas(root,height=100,width = 50,bg = "White").grid(padx = 5,row=0,rowspan=3,columnspan=2, column=0,sticky="EW")
+
+    tk.Button(root, text="Start", width=10, command=None).grid(padx = 5,pady = 5,row=0, column=2)
+    tk.Button(root, text="Stop", width=10, command=None).grid(padx = 5,pady = 5, row=1, column=2)
+    tk.Button(root, text="Config file", width=10, command=None).grid(padx = 5,pady = 5, row=2, column=2)
+
+    label = tk.Label(root, text="Badu Rate:").grid(padx = 5,pady = 5, row=3, column=0, sticky='NW')
+    
+    badu = ttk.Combobox(root,value = baduRate)
+    badu.grid(padx = 0,pady = 5, row=3, column=1)
+    badu.current(0)
+    badu.bind("<<ComboboxSelected>>",None)
+    
+    label = tk.Label(root, text="Port:").grid(padx = 5,pady = 5, row=4, column=0, sticky='NW')
+
+    comList = serial_ports()
+    if not comList:
+        comList.append("No device")
+    com = ttk.Combobox(root,value = comList)
+    com.grid(padx = 5,pady = 5, row=4, column=1)
+    com.current(0)
+    com.bind("<<ComboboxSelected>>",None)
+
+    tk.Button(root, text="Refresh Port", width=10, command=refreshPorts).grid(padx = 5,pady = 5, row=4, column=2)
     root.mainloop()
     
