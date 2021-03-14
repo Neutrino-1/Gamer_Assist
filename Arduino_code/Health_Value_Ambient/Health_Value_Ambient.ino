487fed647071f16fdc83398abe3753f512b7d0ac
Neutrino <srinivasanm329@gmail.com>
2021-03-14 18:05:39 +0530

    ambient  - number of led fix

--- Arduino_code/Health_Value_Ambient/Health_Value_Ambient.ino
+++ Arduino_code/Health_Value_Ambient/Health_Value_Ambient.ino
@@ -1,9 +1,9 @@
-#include <FastLED.h>
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <FastLED.h>
 
 // How many leds in your strip?
-#define NUM_LEDS 13
+#define NUM_LEDS 5
 
 #define DATA_PIN 4
 #define CLOCK_PIN 13
@@ -32,7 +32,7 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&health_S, incomingData, sizeof(health_S));
   currentHealth  = health_S.health;
   Serial.println("REC data");
-  Serial.println(health_S.health);
+  Serial.println(currentHealth);
   trigger = true; 
 }
  
@@ -72,7 +72,7 @@ void GreenToRedGradient(double percentage)
     red = 255*(100-percentage)/100;
     green = 255*(percentage/100);
     blue = 0;
-    for(int i = 0; i < 13; i++)
+    for(int i = 0; i < 5; i++)
     {
       leds[i].red   = red;
       leds[i].green = green;
